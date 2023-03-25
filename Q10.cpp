2bb69f3aacaedf7773b7632c26cbe9064d6fc273
Ashish Garg <ashishgarg5006@gmail.com>
2023-03-25 16:28:33 +0530

    update

---
+++ Q10.cpp
@@ -0,0 +1,24 @@
+// Update the '_' in the code below
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        int A, B, S, P;
+        cin >> A >> B;
+        ;
+        // Sum of inputs
+        S = A _ B;
+        // Product of inputs
+        P = A _ B;
+        // Print the desired output for each test case
+        cout__ << " " << __ << endl;
+    }
+    return 0;
+}
---
+++ Q11.cpp
@@ -0,0 +1,20 @@
+// Update the code below to solve the problem
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        int X, Y;
+        cin >> X;
+        Y = 5 * (2 * (X));
+        cout << Y << endl;
+    }
+
+    return 0;
+}
