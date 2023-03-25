df4fc6d40b23d9b1a84b9752903c3499c72b385d
Ashish Garg <ashishgarg5006@gmail.com>
2023-03-25 10:29:42 +0530

    initial commit

---
+++ Q2.cpp
@@ -0,0 +1,16 @@
+// How to accept string inputs
+// Update the '_' in the code below
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string A, B, C, D, E, F, G, H, I;
+    cin >> A >> B;
+    cin >> C >> D >> E;
+    cin >> F >> G >> H >> I;
+    cout << A << " " << B << " " << C << " " << D << " " << E << " " << F << " " << G << " " << H << " " << I;
+    return 0;
+}
---
+++ Q4.cpp
@@ -0,0 +1,16 @@
+// Input mirror
+// Update the '_' in the code below
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int N;
+    string S;
+    cin >> N;
+    cin >> S;
+    cout << N <<" "<< S;
+    return 0;
+}
